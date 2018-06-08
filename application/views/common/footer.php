9a72be0f15e03fc29b6af4b69dd43f4eda0aff9d
christianlimanto95 <christianlimanto95@gmail.com>
2018-06-08 14:45:14 +0700

    tawk.to

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -17,5 +17,18 @@
 <script src="<?php echo base_url("assets/js/common/default.js?v=15"); ?>" defer></script>
 <?php echo $additional_js; ?>
 <script src="<?php echo base_url("assets/js/" . $page_name . ".js?v=19"); ?>" defer></script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5b1a31a910b99c7b36d4bdfd/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 </html>
